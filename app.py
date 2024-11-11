a2d137ab83cc249de22fe94b371d1e420e1051cb
Daniel Handoyo <daniel023@e.ntu.edu.sg>
2024-11-11 17:33:37 +0800

    Updated dashboard

--- app.py
+++ app.py
@@ -11,9 +11,21 @@ from collections import Counter
 import re
 
 
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+
+try:
+    nltk.data.find('corpora/wordnet')
+except LookupError:
+    nltk.download('wordnet')
+
 
 lemmatizer = WordNetLemmatizer()
 analyzer = SentimentIntensityAnalyzer()
